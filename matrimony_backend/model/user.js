4fbd91feb2e374d19b98d15a929f0cefa5a7f213
veerendrasoni07 <veerendrasoni0555@gmail.com>
2025-08-18 11:41:48 +0530

    user model updated

--- matrimony_backend/model/user.js
+++ matrimony_backend/model/user.js
@@ -5,6 +5,81 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    dob:{
+        type:String,
+        required:false
+    },
+    pob:{
+        type:String,
+        required:false
+    },
+    height:{
+        type:String,
+        required:false
+    },
+    weight:{
+        type:String,
+        required:false
+    },
+    marital_status:{
+        type:String,
+        enum:['Single','Widowed','Divorced']
+    },
+    state:{
+        type:String,
+    },
+    city:{
+        type:String
+    },
+    address:{
+        type:String
+    },
+    religion:{
+        type:String
+    },
+    caste:{
+        type:String
+    },
+    subcaste:{
+        type:String
+    },
+    mother_tongue:{
+        type:String,
+    },
+    gender:{
+        type:String,
+        enum:['Male','Female','Others']
+    },
+    education:{
+        type:String
+    },
+    occupation:{
+        type:String
+    },
+    worksWith:{
+        type:String
+    },
+    workAS:{
+        type:String
+    },
+    annual_income:{
+        type:Number
+    },
+    diet:{
+        type:String,
+        enum: ['Veg','Non-Veg']
+    },
+    family_type:{
+        type:String,
+        enum:['Joint','Nuclear']
+    },
+    image:{
+        type:String,
+    },
+    phone:{
+        type:Number
+    },
+
     email:{
         type:String,
         required:true,
